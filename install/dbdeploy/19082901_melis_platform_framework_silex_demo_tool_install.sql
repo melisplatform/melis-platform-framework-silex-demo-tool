75e3a99af91cdea14acb269f7be27aec95d982ed
Salcover <simonalcover@gmail.com>
2019-09-03 12:52:39 +0800

    update db deploy

--- install/dbdeploy/19082901_melis_platform_framework_silex_demo_tool_install.sql
+++ install/dbdeploy/19082901_melis_platform_framework_silex_demo_tool_install.sql
@@ -7,26 +7,17 @@
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.2.15
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `lumen`
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `album`
 --
+DROP TABLE IF EXISTS `album` ;
 
 CREATE TABLE `album` (
   `alb_id` int(11) NOT NULL,
