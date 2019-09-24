90280562522c65f5f25118d567e50c2c24a7e756
Salcover <simonalcover@gmail.com>
2019-09-24 12:46:57 +0800

    Ticket # : 4197 (dev6 - page edition - Silex plugin name)
    (cherry picked from commit e7cd14c)

--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -7,6 +7,6 @@
         'tr_melisplatformsilexdemotool_album_date_added' => 'Date Added',
         'tr_melisplatformsilexdemotool_tool_title' => 'Un embryon d\'outil fait en Silex',
         'tr_melisplatformsilexdemotool_tool_desc' => 'Ce outil de demo a pour but de montrer comment utiliser le framework Silex à l\'intérieur de Melis Platform, et ne pas être bloqué en ZF.',
-        'tr_melisplatformsilexdemotool_plugin_name' => 'Outil Demo en Silex',
+        'tr_melisplatformsilexdemotool_plugin_name' => 'Silex Plugin',
         'tr_melisplatformsilexdemotool_plugin_Description' => 'Ce plugin affiche du contenu tiré de Silex.',
     ];
